173c2a52f322df06cced5d55d4cac31d5c21fbde
thekikoman <kikoknowsyouall@gmail.com>
2021-11-01 16:28:28 -0700

    added beta settings

---
+++ settings.js
@@ -0,0 +1,10 @@
+let allCookies = document.cookie.split('; ');
+
+let settings = {
+	betaMode: allCookies.find(row => row.startsWith('betaMode')).split('=')[1]
+}
+
+if (settings.betaMode === 'true'){
+	document.getElementById("globalBackground").innerHTML ="<video autoplay loop muted poster='/res/bg1.png' id='flarebg'><source src='/res/flarebg.webm' type='video/webm'></video>";
+}
+
