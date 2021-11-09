eaad38a93df15d7e4dc9b8600b19ecd50572b1d4
thekikoman <kikoknowsyouall@gmail.com>
2021-11-09 14:41:29 -0800

    created artwork viewer page

---
+++ artworkviewer.js
@@ -0,0 +1,52 @@
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let image = urlParams.get('i');
+
+let png = new Image();
+png.src = '/art-images/' + image + '.png';
+
+
+//Loads Image. I did not write this code for the most part.
+png.onload = function() {
+    // image exists and is loaded
+    document.getElementsByClassName("bigImageHolder")[0].appendChild(png);
+	let imageLink = 'https://flare145.com/art-images/' + image + '.png';
+}
+png.onerror = function() {
+    // image did not load
+
+    let jpg = new Image();
+    jpg.src = '/art-images/' + image + '.jpg';
+
+    document.getElementsByClassName("bigImageHolder")[0].appendChild(jpg);
+	imageLink = 'https://flare145.com/art-images/' + image + '.jpg';
+}
+
+let imageInfo = {};
+
+//this was copied from mozilla docs. no clue how it works but it reads files
+fetch('https://flare145.com/image-info/' + image + '.json')
+  .then(response => response.json())
+  .then(data => {
+	imageInfo = data;
+	let title = document.createElement("p");
+	title.innerText = imageInfo.title;
+	document.getElementsByClassName("details")[0].appendChild(title);
+	let medium = document.createElement("p");
+	medium.innerText = imageInfo.medium;
+	document.getElementsByClassName("details")[0].appendChild(medium);
+	let resolution = document.createElement("p");
+	resolution.innerText = imageInfo.resolution;
+	document.getElementsByClassName("details")[0].appendChild(resolution);
+	let size = document.createElement("p");
+	size.innerText = imageInfo.size;
+	document.getElementsByClassName("details")[0].appendChild(size);
+	let description = document.createElement("p");
+	description.innerText = imageInfo.description;
+	document.getElementsByClassName("details")[0].appendChild(description);
+});
+
+function linkTo() {
+	window.open(imageLink, '_blank');
+}
+
